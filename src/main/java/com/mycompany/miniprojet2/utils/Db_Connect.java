baf3ab14f604fcaa86fcf09cf7315ad87524312e
Maison <Maison@Maison-PC>
2020-04-30 13:52:03 -0400

    reste genarateur identifiant

--- src/main/java/com/mycompany/miniprojet2/utils/Db_Connect.java
+++ src/main/java/com/mycompany/miniprojet2/utils/Db_Connect.java
@@ -18,7 +18,7 @@ public class Db_Connect {
        
         this.url = "jdbc:mysql://localhost:3306/db_miniprojetii_web?useUnicode=true&useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         this.user = "root";
-        this.pass = "014Laca3016";
+        this.pass = "Boston12a";
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
